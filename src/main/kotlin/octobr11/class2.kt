3e3d86f3dcafe030bd04236d698786105d23d2f2
lenovo <farrukh1994@naver.com>
2022-10-12 02:39:45 +0900

    [::add] class

--- src/main/kotlin/octobr11/class2.kt
+++ src/main/kotlin/octobr11/class2.kt
@@ -1,2 +1,19 @@
 package octobr11
 
+class Cup2 {
+    var percentFull = 0
+    val max = 100
+    fun add(increase: Int): Int {
+        percentFull += increase
+        if (percentFull > max)
+            percentFull = max
+        return percentFull
+    }
+}
+fun main() {
+    val cup = Cup2()
+    cup.add(50)
+    println(cup.percentFull)
+    cup.add(70)
+    println(cup.percentFull)
+}
