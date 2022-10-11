625dbcbed34c9552d6435b88a27d82c56afc3520
lenovo <farrukh1994@naver.com>
2022-10-11 12:51:11 +0900

    [::add] class

---
+++ src/main/kotlin/octobr11/class1.kt
@@ -0,0 +1,13 @@
+package octobr11
+
+class Radiotion() {
+    fun Coution () = " warning "
+    fun exercise() =
+        this.Coution () +
+                Coution() +
+                " There is radiotion  "
+}
+ fun main(){
+     val radiotion=Radiotion()
+     println (radiotion.exercise())
+ }
