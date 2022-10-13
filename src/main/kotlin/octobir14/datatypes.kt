b88693dd0c3178e0f91b100b02ee03d8571a5f61
lenovo <farrukh1994@naver.com>
2022-10-13 15:32:31 +0900

    [::add] data types

---
+++ src/main/kotlin/octobir14/datatypes.kt
@@ -0,0 +1,16 @@
+package octobir14
+
+fun main() {
+    val whole: Int = 11
+    val fractional: Double = 1.4
+    val true0False: Boolean = true
+    val words: String = "A value"
+    val character: Char = 'a'
+    val lines: String = """Triple quotes let you have many lines in you have many lines  """
+    println(whole)
+    println(fractional)
+    println(true0False)
+    println(words)
+    println(character)
+    println(lines)
+}
