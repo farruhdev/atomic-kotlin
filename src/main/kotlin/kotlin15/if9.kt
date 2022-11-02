a74d4b61cc2ec0cf6ba93e4a86e14a5f6afd7feb
lenovo <farrukh1994@naver.com>
2022-11-03 04:06:52 +0900

    [::add] if3.kt

---
+++ src/main/kotlin/kotlin15/if9.kt
@@ -0,0 +1,16 @@
+package kotlin15
+
+fun main(args: Array<String>) {
+
+    val a = 2
+    val b = 5
+
+    var maxValue:Int
+
+    if (a > b)
+        maxValue = a
+    else
+        maxValue = b
+
+    println(maxValue)
+}
