4d47574887fdf60572604ac28f065acaa1edd053
lenovo <farrukh1994@naver.com>
2022-11-07 08:25:48 +0900

    [::add] if8

---
+++ src/main/kotlin/kotlin15/while8.kt
@@ -0,0 +1,11 @@
+package kotlin15
+
+  fun main() {
+
+      var y = 1
+      while (y <= 10) {
+          print("$y ")
+      }
+  }
+ //While 조건을 <=10 으로 둘 때 아래와 같은 결과를 얻을 수 있다. 이 때 y++는 Increment Operator로 1씩 더하는 것을 의미한다.
+//[출처] 코틀린(Kotlin)_While과 For|작성자 책읽는 공돌이
