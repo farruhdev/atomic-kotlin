eb89f38120c667bd118bd9567f021a4535b29235
lenovo <farrukh1994@naver.com>
2022-11-07 02:40:47 +0900

    [::add] if11.kt

---
+++ src/main/kotlin/kotlin15/if11.kt
@@ -0,0 +1,13 @@
+package kotlin15
+
+fun main() {
+    val stuScore2 = 60
+
+    if(stuScore2 > 50) {
+        println("50 초과")
+    } else if (stuScore2 >= 50) {
+        println("50 이상")
+    } else {
+        println("50 미만")
+    }
+}
