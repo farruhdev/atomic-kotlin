780d78c46c2b2246104c96280c5116d8c21c5911
lenovo <farrukh1994@naver.com>
2022-11-09 03:16:52 +0900

    [::add] Loopandranges9.kt

---
+++ src/main/kotlin/kotlin15/Loopandranges9.kt
@@ -0,0 +1,15 @@
+package kotlin15
+
+fun main() {
+   // for(i in 10 downTo 1 step 2){
+
+  //  }
+    val number = 2
+    for(i in 1..10)
+    {
+       // 2 x 1 = 2
+       // 2 x 2 = 4
+        println(number.toString() + " x " + i + " = " + (number * i))
+
+    }
+}
