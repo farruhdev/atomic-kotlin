bb42a235578c6b72854e1fdfa481a2255a609cb7
lenovo <farrukh1994@naver.com>
2022-11-09 03:04:57 +0900

    [::add] Loopandranges8.kt

---
+++ src/main/kotlin/kotlin15/Loopandrange8.kt
@@ -0,0 +1,16 @@
+package kotlin15
+
+fun main () {
+  //  for(i in 1..5 step 2) {
+    //    println(i)
+  //  }
+
+  //  println("Until")
+  //  for(i in 1 until 5) {
+  //      println(i)
+  //  }
+    for(i in 10 downTo 1){
+        println(i)
+    }
+
+}
