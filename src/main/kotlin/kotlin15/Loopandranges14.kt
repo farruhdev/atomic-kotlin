80f2b7bf1df73c863a6c57462248bbb287d1e403
lenovo <farrukh1994@naver.com>
2022-11-09 05:27:56 +0900

    [::add] Loopandranges14.kt

---
+++ src/main/kotlin/kotlin15/Loopandranges14.kt
@@ -0,0 +1,14 @@
+package kotlin15
+
+fun main() {
+   // val reverseRange: CharProgression = 'e' downTo 'a'
+   // for (range: Char in reverseRange) {
+   //   print(range)
+  //  }
+
+    val range: CharRange = 'a'..'d'
+    for (item: Char in range) {
+        print(item)
+    }
+}
+
