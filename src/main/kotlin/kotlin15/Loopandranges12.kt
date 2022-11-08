4b3d0a931aa06959c42ce2a1939b347304892856
lenovo <farrukh1994@naver.com>
2022-11-09 05:16:34 +0900

    [::add] Loopandranges12.kt

---
+++ src/main/kotlin/kotlin15/Loopandranges12.kt
@@ -0,0 +1,16 @@
+package kotlin15
+
+fun main() {
+    // for, while, do-while
+    // 1 -> 10
+    for  (item in 1..10) {
+           print("$item ->")
+    }
+
+    println()
+
+    for (item in 1 until 10) {
+        print("$item ->")
+    }
+
+}
