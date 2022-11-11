8b27bfe634da4f111ef876ed21b39be190a6ec3d
lenovo <farrukh1994@naver.com>
2022-11-12 08:51:49 +0900

    [::add] List13.kt

---
+++ src/main/kotlin/kotlin15/List13.kt
@@ -0,0 +1,14 @@
+package kotlin15
+
+fun printAll(strings: Collection<String>) {
+    for(s in strings) print("$s ")
+    println()
+}
+
+fun main() {
+    val stringList = listOf("one", "two", "one")
+    printAll(stringList)
+
+    val stringSet = setOf("one", "two", "three")
+    printAll(stringSet)
+}
