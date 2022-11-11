9dc6e21766ccc1af135e33ac2c01238d21bbacad
lenovo <farrukh1994@naver.com>
2022-11-12 08:53:45 +0900

    [::add] List14.kt

---
+++ src/main/kotlin/kotlin15/List14.kt
@@ -0,0 +1,11 @@
+package kotlin15
+
+fun main() {
+
+    val bob = Person("Bob", 31)
+    val people = listOf(Person("Adam", 20), bob, bob)
+    val people2 = listOf(Person("Adam", 20), Person("Bob", 31), bob)
+    println(people == people2)
+    bob.age = 32
+    println(people == people2)
+}
