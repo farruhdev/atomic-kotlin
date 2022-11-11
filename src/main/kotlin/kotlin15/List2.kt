0af800ce9a7dd53cb15675a6eaf5e9422e74043a
lenovo <farrukh1994@naver.com>
2022-11-12 08:09:58 +0900

    [::add] List2.kt

---
+++ src/main/kotlin/kotlin15/List2.kt
@@ -0,0 +1,12 @@
+package kotlin15
+
+fun main(args: Array<String>) {
+    //initialize a mutable list
+    var listA = mutableListOf("Example", "Program", "Tutorial")
+
+    //add item to the list
+    listA.add("Sample")
+
+    //print the list
+    println(listA)
+}
