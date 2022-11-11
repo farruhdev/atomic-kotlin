2e5ecefdee6803db8f380690dc1bc7fba3e7a3d2
lenovo <farrukh1994@naver.com>
2022-11-12 08:26:16 +0900

    [::add] List4.kt

---
+++ src/main/kotlin/kotlin15/List4.kt
@@ -0,0 +1,11 @@
+package kotlin15
+
+fun main(args: Array<String>) {
+    //initialize a list
+    var listB = listOf<String>("Example", "Program", "Tutorial")
+
+    //for loop on list
+    for(item in listB){
+        println(item)
+    }
+}
