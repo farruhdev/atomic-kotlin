dd756eb50fe3ecf3a652632ec0c0885aae93c73d
lenovo <farrukh1994@naver.com>
2022-11-14 01:02:34 +0900

    [::add] Sets14.kt

---
+++ src/main/kotlin/kotlin15/Set14.kt
@@ -0,0 +1,11 @@
+package kotlin15
+
+fun main() {
+    // Declaring a mutable set with different types
+    val mySet: Set<Any> = mutableSetOf(1, "a", "Developers", 12.3)
+
+    println("Display the values in set")
+
+    // Print the elements in the set
+    println(mySet)
+}
