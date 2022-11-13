315147d9b9147ddc0d78ad5cc2d80cac31fdff05
lenovo <farrukh1994@naver.com>
2022-11-14 00:46:09 +0900

    [::add] Sets10.kt

---
+++ src/main/kotlin/kotlin15/Set10.kt
@@ -0,0 +1,16 @@
+package kotlin15
+
+fun main(args: Array<String>)
+{
+    var set1 = setOf(1, 2, 3, 4, 5, 6, 7, 8, );
+
+    for (item in set1)
+    {
+        println(item);
+    }
+
+    var set2: Set<String> = setOf("Hello", "World", "4");
+    var set3: Set<Any> = setOf(1, 2, "farrux", true);
+
+    println(set3.contains(4));
+}
